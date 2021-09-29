459a6540f24275240b870e57f9576b6135c47659
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-09-29 22:18:45 +0200

    Move SVGs to external files

--- src/MenuItem.ts
+++ src/MenuItem.ts
@@ -1,5 +1,6 @@
 import style from './style.scss';
 import Menu from './Menu';
+import svg from './svg.json';
 
 export default class MenuItem {
   element: HTMLDivElement;
@@ -11,8 +12,7 @@ export default class MenuItem {
 
     if (menuItem.role == 'mainMenu') {
       // Add main menu svg
-      this.element.innerHTML =
-        '<svg width="10" height="10" viewBox="0 0 384 384"><rect x="0" y="277.333" width="384" height="42.667"/><rect x="0" y="170.667" width="384" height="42.667"/><rect x="0" y="64" width="384" height="42.667"/></svg>';
+      this.element.innerHTML = svg.mainMenu;
     }
 
     if (menuItem.label) {
@@ -59,7 +59,7 @@ export default class MenuItem {
     }
     if (menuItem.checked) {
       // Add check mark
-      this.element.innerHTML += `<svg class="${style.locals.check}" version="1.1" width="12px" height="12px" viewBox="0 0 512 512"><path d="M504.502,75.496c-9.997-9.998-26.205-9.998-36.204,0L161.594,382.203L43.702,264.311c-9.997-9.998-26.205-9.997-36.204,0c-9.998,9.997-9.998,26.205,0,36.203l135.994,135.992c9.994,9.997,26.214,9.99,36.204,0L504.502,111.7C514.5,101.703,514.499,85.494,504.502,75.496z"/></svg>`;
+      this.element.innerHTML += svg.check.replace('{class}', style.locals.check);
     }
 
     switch (menuItem.type) {
@@ -78,7 +78,7 @@ export default class MenuItem {
         break;
       case 'submenu':
         // Add right arrow
-        this.element.innerHTML += `<svg class="${style.locals.arrow}" version="1.1" width="20px" height="20px" viewBox="0 0 24 24"><path d="M9.29,6.71L9.29,6.71c-0.39,0.39-0.39,1.02,0,1.41L13.17,12l-3.88,3.88c-0.39,0.39-0.39,1.02,0,1.41l0,0c0.39,0.39,1.02,0.39,1.41,0l4.59-4.59c0.39-0.39,0.39-1.02,0-1.41l-4.59-4.59C10.32,6.32,9.68,6.32,9.29,6.71z" /></svg>`;
+        this.element.innerHTML += svg.arrow.replace('{class}', style.locals.arrow);
 
         this.element.onclick = (e) => {
           e.stopPropagation();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import style from './style.scss';
 import Menu from './Menu';
+import svg from './svg.json';
 
 let container: HTMLDivElement;
 let titlebar: HTMLDivElement;
@@ -71,25 +72,22 @@ export default class Titlebar {
     minimizeWindow = document.createElement('div');
     /*minimizeWindow.id = 'minimize';*/
     minimizeWindow.classList.add(style.locals.button);
-    minimizeWindow.innerHTML =
-      '<svg x="0px" y="0px" viewBox="0 0 10.2 1"><rect x="0" y="50%" width="10.2" height="1" /></svg>';
+    minimizeWindow.innerHTML = svg.windows.minimize;
     controls.append(minimizeWindow);
     maximizeWindow = document.createElement('div');
     maximizeWindow.id = style.locals.maximize;
     maximizeWindow.classList.add(style.locals.button);
-    maximizeWindow.innerHTML = '<svg viewBox="0 0 10 10"><path d="M0,0v10h10V0H0z M9,9H1V1h8V9z" /></svg>';
+    maximizeWindow.innerHTML = svg.windows.maximize;
     controls.append(maximizeWindow);
     restoreWindow = document.createElement('div');
     restoreWindow.id = style.locals.restore;
     restoreWindow.classList.add(style.locals.button);
-    restoreWindow.innerHTML =
-      '<svg viewBox="0 0 10.2 10.1"><path d="M2.1,0v2H0v8.1h8.2v-2h2V0H2.1z M7.2,9.2H1.1V3h6.1V9.2z M9.2,7.1h-1V2H3.1V1h6.1V7.1z" /></svg>';
+    restoreWindow.innerHTML = svg.windows.restore;
     controls.append(restoreWindow);
     closeWindow = document.createElement('div');
     closeWindow.id = style.locals.close;
     closeWindow.classList.add(style.locals.button);
-    closeWindow.innerHTML =
-      '<svg viewBox="0 0 10 10"><polygon points="10.2,0.7 9.5,0 5.1,4.4 0.7,0 0,0.7 4.4,5.1 0,9.5 0.7,10.2 5.1,5.8 9.5,10.2 10.2,9.5 5.8,5.1" /></svg>';
+    closeWindow.innerHTML = svg.windows.close;
     controls.append(closeWindow);
     titlebar.append(controls);
 
