977243698d1cb631f38538aec062ab44420bf04e
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-12-20 22:17:40 +0100

    Update ClickHandler

--- src/classes/ClickHandler.ts
+++ src/classes/ClickHandler.ts
@@ -1,24 +1,29 @@
 import { Options } from './Options';
 
 export const ClickHandler = {
-  click(menuItem: Record<string, any>): void {
+  click(menuItem: Record<string, any>, accelerator=false): void {
     if (Options.values.menuItemClickHandler && menuItem.commandId) {
       // Use user-defined handler
       Options.values.menuItemClickHandler(menuItem.commandId);
     } else if (menuItem.click) {
       // Use default handler
+      const keyboardEvent = {
+        triggeredByAccelerator: accelerator
+      }
+
       if (menuItem.click.toString().indexOf('ipcRenderer') > 0) {
         // Invoke electron click method
         menuItem.click(
-          {}, // KeyboardEvent
+          keyboardEvent, // KeyboardEvent
           Options.values.browserWindow ? Options.values.browserWindow : null, // BrowserWindow
+          Options.values.browserWindow ? Options.values.browserWindow.webContents : null, // WebContents
         );
       } else {
         // Invoke custom template click method
         menuItem.click(
           menuItem, // MenuItem
           null, // BrowserWindow
-          {}, // KeyboardEvent
+          keyboardEvent, // KeyboardEvent
         );
       }
     }
